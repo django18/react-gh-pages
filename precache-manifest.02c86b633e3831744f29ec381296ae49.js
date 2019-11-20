a232a2666cc2b8ee8dc0428ce62126715b115c66
Avinash Dangi <avinashdangi.26@gmail.com>
2019-11-20 21:32:34 +0530

    Updates

---
+++ precache-manifest.02c86b633e3831744f29ec381296ae49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dab4954cd391190c77f6d678c0fb2d",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "6735a63153b2d0ea7d65",
+    "url": "/pomodoro-clock/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fb0c9ca5cbd80fe2a91",
+    "url": "/pomodoro-clock/static/js/2.e1806175.chunk.js"
+  },
+  {
+    "revision": "6735a63153b2d0ea7d65",
+    "url": "/pomodoro-clock/static/js/main.1d4af7ab.chunk.js"
+  },
+  {
+    "revision": "0c81f513fa6de9357668",
+    "url": "/pomodoro-clock/static/js/runtime-main.c9c7a64f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pomodoro-clock/static/media/logo.25bf045c.svg"
+  }
+]);
